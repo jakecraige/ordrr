eb1d38a19f06bf63d602c5cc7dc9c4e3484b9506
Jake Craige <jake@poeticsystems.com>
2014-08-30 17:29:16 -0500

    add attributes to order model

--- app/models/order.js
+++ app/models/order.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
+var attr = DS.attr;
+
 export default DS.Model.extend({
-  
+  description: attr(),
+  receiver: attr(),
+  ready: attr('boolean', { default: false })
 });
