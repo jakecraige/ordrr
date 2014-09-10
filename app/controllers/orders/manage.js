d82c77e1bbb97ee42eed5dd985bf8ce4f0d225d2
Jake Craige <jake@poeticsystems.com>
2014-09-10 18:55:31 -0500

    jshint fix

--- app/controllers/orders/manage.js
+++ app/controllers/orders/manage.js
@@ -4,7 +4,7 @@ export default Ember.ArrayController.extend({
   actions: {
     ready: function(order) {
       order.set('ready', true);
-      order.save().then(this.removeObject.bind(this))
+      order.save().then(this.removeObject.bind(this));
     }
   }
 });
